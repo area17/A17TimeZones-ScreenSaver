6aec0b02b5572f3191d28e5bb9b5ba64e692c189
Mike Byrne <mike@thirteentwelve.com>
2017-10-07 17:04:22 +0100

    Can pass through a URL be displayed

--- A17TimeZones/A17TimeZones.m
+++ A17TimeZones/A17TimeZones.m
@@ -5,7 +5,11 @@
 #import "A17TimeZones.h"
 #import <WebKit/WebKit.h>
 
+
 @implementation A17TimeZones
+
+NSString * static_url = @"file://%@/www/index.html?url=";
+NSString * base_url = @"http@3A@2F@2Fa17_timezones.localip@2F@3Fscreensaver@3Dtrue";
 
 - (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
 {
@@ -14,7 +18,7 @@
     if (self) {
         webView = [[WebView alloc] initWithFrame:[self bounds] frameName:nil groupName:nil];
 
-        [webView setMainFrameURL:[NSString stringWithFormat:@"file://%@/www/index.html", [[NSBundle bundleForClass:[self class]] resourcePath]]];
+        [webView setMainFrameURL:[NSString stringWithFormat:[static_url stringByAppendingString:base_url], [[NSBundle bundleForClass:[self class]] resourcePath]]];
 
         [self addSubview:webView];
     }
