21ea335654e76a9568634ff9c0bbbb11f41dc4dd
Mike Byrne <mike@thirteentwelve.com>
2017-10-08 19:06:18 +0100

    adding gitignore

--- A17TimeZones/A17TimeZones.m
+++ A17TimeZones/A17TimeZones.m
@@ -5,6 +5,7 @@
 #import "A17TimeZones.h"
 #import <WebKit/WebKit.h>
 #define ModuleName @"com.github.area17.A17TimeZones"
+#define NSStringFromBOOL(aBOOL)    ((aBOOL) ? @"YES" : @"NO")
 
 @implementation A17TimeZones
 
@@ -28,28 +29,27 @@ NSString * base_url = @"http@3A@2F@2Fa17_timezones.localip@2F@3Fscreensaver@3Dtr
         
         NSString * paramClockType = @"@26clocktype@3D";
         NSString * paramDigitalFormat = @"@26digitalformat@3D";
-        //NSString * paramTemperature = @"@26temperature@3D";
+        NSString * paramTemperature = @"@26temperature@3D";
         NSString * paramTemperatureUnits = @"@26TemperatureUnits@3D";
-        //NSString * paramWeather = @"@26weather@3D";
+        NSString * paramWeather = @"@26weather@3D";
         
         NSString * valueClockType = [[defaults objectForKey:@"ClockType"] lowercaseString];
         NSString * valueDigitalFormat = [[defaults objectForKey:@"DigitalFormat"] lowercaseString];
-        //NSString * valueTemperature = [[defaults objectForKey:@"Temperature"] lowercaseString];
+        NSString * valueTemperature = [NSStringFromBOOL([defaults objectForKey:@"Temperature"]) lowercaseString];
         NSString * valueTemperatureUnits = [[defaults objectForKey:@"TemperatureUnits"] lowercaseString];
-        //NSString * valueWeather = [[defaults objectForKey:@"Weather"] lowercaseString];
+        NSString * valueWeather = [NSStringFromBOOL([defaults objectForKey:@"Weather"]) lowercaseString];
         
         paramClockType = [paramClockType stringByAppendingString:valueClockType];
         paramDigitalFormat = [paramDigitalFormat stringByAppendingString:valueDigitalFormat];
-        //paramTemperature = [paramTemperature stringByAppendingString:valueTemperature];
+        paramTemperature = [paramTemperature stringByAppendingString:valueTemperature];
         paramTemperatureUnits = [paramTemperatureUnits stringByAppendingString:valueTemperatureUnits];
-        //paramWeather = [paramWeather stringByAppendingString:valueWeather];
+        paramWeather = [paramWeather stringByAppendingString:valueWeather];
         
         url = [url stringByAppendingString:paramClockType];
         url = [url stringByAppendingString:paramDigitalFormat];
-        //url = [url stringByAppendingString:paramTemperature];
+        url = [url stringByAppendingString:paramTemperature];
         url = [url stringByAppendingString:paramTemperatureUnits];
-        //url = [url stringByAppendingString:paramWeather];
-        /**/
+        url = [url stringByAppendingString:paramWeather];
         
         webView = [[WebView alloc] initWithFrame:[self bounds] frameName:nil groupName:nil];
         
